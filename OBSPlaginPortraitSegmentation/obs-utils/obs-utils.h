2ebb726db0aef5e461b2cc2696d7904859726fa1
L3b1n <binleo07@icloud.com>
2024-08-25 17:44:33 +0300

    Delete some files to have cleanup

--- OBSPlaginPortraitSegmentation/obs-utils/obs-utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef OBS_UTILS_H
-#define OBS_UTILS_H
-
-#include "../FilterData.h"
-
-bool getRGBAFromStageSurface(filter_data *tf, uint32_t &width, uint32_t &height);
-
-#endif /* OBS_UTILS_H */
