9cd770056030b557df8825e28da135bcdcea54de
L3b1n <binleo07@icloud.com>
2024-08-25 20:38:08 +0300

    Move working files to another folder for more friendly file structure

---
+++ OBSPlaginPortraitSegmentation/src/FilterData.h
@@ -0,0 +1,44 @@
+#ifndef FILTERDATA_H
+#define FILTERDATA_H
+
+#include "models/Model.h"
+#include "ort-utils/ORTModelData.h"
+
+/**
+  * @brief The filter_data struct
+  *
+  * This struct is used to store the base data needed for ORT filters.
+  *
+*/
+struct FilterData : public ORTModelData {
+	std::string useGPU;
+	uint32_t numThreads;
+	std::string modelSelection;
+	std::unique_ptr<Model> model;
+
+	cv::Mat inputRGBA;
+
+	bool isDisabled;
+
+	std::mutex inputRGBALock;
+	std::mutex outputLock;
+
+	const unsigned char* modelInfo = nullptr;
+    unsigned int modelSize = 0;
+};
+
+struct BackgroundRemovalFilter : public FilterData {
+    bool enableThreshold = true;
+    float threshold = 0.5f;
+    cv::Scalar backgroundColor{0, 0, 0, 0};
+    float contourFilter = 0.05f;
+    float smoothContour = 0.5f;
+    float feather = 0.0f;
+
+    cv::Mat backgroundMask;
+    int maskEveryXFrames = 1;
+    int maskEveryXFramesCount = 0;
+    int64_t blurBackground = 0;
+};
+
+#endif /* FILTERDATA_H */
