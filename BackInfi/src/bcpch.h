da625fa26bbe5507edba031a649d4c1358107792
L3b1n <binleo07@icloud.com>
2024-08-26 23:22:49 +0300

    Add precompiled header files for the project

---
+++ BackInfi/src/bcpch.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef NOMINMAX
+	// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
+	#define NOMINMAX
+#endif
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "BackInfi/Core/Base.h"
+
+#include "BackInfi/Core/Logger.h"
+
+#ifdef BC_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
