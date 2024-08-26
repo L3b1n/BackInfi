57623ef520324145576a0359ec5b234fd2de094d
L3b1n <binleo07@icloud.com>
2024-08-26 22:30:45 +0300

    Add premake file for generating visual studio projects

---
+++ BackInfi/premake5.lua
@@ -0,0 +1,143 @@
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["glm"]         = "vendor/glm"
+IncludeDir["GLFW"]        = "vendor/GLFW/include"
+IncludeDir["Glad"]        = "vendor/Glad/include"
+IncludeDir["Opencv"]      = "vendor/Opencv/include"
+IncludeDir["Onnxruntime"] = "vendor/Onnxruntime/include"
+
+LibsDir = {}
+LibsDir["Opencv"]         = "vendor/Opencv/Lib"
+LibsDir["Onnxruntime"]    = "vendor/Onnxruntime/Lib"
+
+include "vendor/GLFW"
+include "vendor/Glad"
+
+project "BackInfi"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "bcpch.h"
+	pchsource "src/bcpch.cpp"
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.inl"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
+	includedirs
+	{
+		"src",
+		"vendor/spdlog/include",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.Opencv}",
+		"%{IncludeDir.Onnxruntime}"
+	}
+
+	libdirs
+	{
+		"%{LibsDir.Opencv}",
+		"%{LibsDir.Onnxruntime}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"gdi32.lib",
+		"Winmm.lib",
+		"Version.lib",
+		"opengl32.lib",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"BC_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+		links
+		{
+			"aded.lib",
+			"IlmImfd.lib",
+			"ippiwd.lib",
+			"ittnotifyd.lib",
+			"libjpeg-turbod.lib",
+			"libopenjp2d.lib",
+			"libpngd.lib",
+			"libprotobufd.lib",
+			"libtiffd.lib",
+			"libwebpd.lib",
+			"opencv_world480d.lib",
+			"quircd.lib",
+			"zlibd.lib",
+			"onnxruntime_d.lib"
+		}
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"ade.lib",
+			"IlmImf.lib",
+			"ippicvmt.lib",
+			"ippiw.lib",
+			"ittnotify.lib",
+			"libjpeg-turbo.lib",
+			"libopenjp2.lib",
+			"libpng.lib",
+			"libprotobuf.lib",
+			"libtiff.lib",
+			"libwebp.lib",
+			"opencv_world480.lib",
+			"quirc.lib",
+			"zlib.lib",
+			"onnxruntime.lib"
+		}
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+
+		links
+		{
+			"ade.lib",
+			"IlmImf.lib",
+			"ippicvmt.lib",
+			"ippiw.lib",
+			"ittnotify.lib",
+			"libjpeg-turbo.lib",
+			"libopenjp2.lib",
+			"libpng.lib",
+			"libprotobuf.lib",
+			"libtiff.lib",
+			"libwebp.lib",
+			"opencv_world480.lib",
+			"quirc.lib",
+			"zlib.lib",
+			"onnxruntime.lib"
+		}
