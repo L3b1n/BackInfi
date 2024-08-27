60462c85e9d8e306168f47a5aa277933bf1f0add
L3b1n <binleo07@icloud.com>
2024-08-27 15:18:23 +0300

    Add PPHumanSeg Model class for inference

---
+++ BackInfi/src/BackInfi/Inference/Models/ModelPPHumanSeg.cpp
@@ -0,0 +1,45 @@
+#include "bcpch.h"
+
+#include "BackInfi/Inference/Models/ModelPPHumanSeg.h"
+
+namespace BackInfi
+{
+    ModelPPHumanSeg::ModelPPHumanSeg()
+    {
+    }
+
+    ModelPPHumanSeg::~ModelPPHumanSeg()
+    {
+    }
+
+    void ModelPPHumanSeg::PrepareInputToNetwork(
+		cv::Mat& resizedImage,
+		cv::Mat& preprocessedImage)
+	{
+		resizedImage = (resizedImage / 256.0 - cv::Scalar(0.5, 0.5, 0.5))
+			/ cv::Scalar(0.5, 0.5, 0.5);
+
+		Utils::HwcToChw(resizedImage, preprocessedImage);
+	}
+
+    cv::Mat ModelPPHumanSeg::GetNetworkOutput(
+		const Activation& activation,
+		const std::vector<std::vector<int64_t>>& outputDims,
+		std::vector<std::vector<float>>& outputTensorValues)
+	{
+		uint32_t outputWidth = (int)outputDims[0].at(2);
+		uint32_t outputHeight = (int)outputDims[0].at(1);
+		int32_t outputChannels = CV_32FC2;
+
+		return cv::Mat(outputHeight, outputWidth, outputChannels, outputTensorValues[0].data());
+	}
+
+    void ModelPPHumanSeg::PostprocessOutput(cv::Mat& outputImage)
+	{
+		// take 1st channel
+		std::vector<cv::Mat> outputImageSplit;
+		cv::split(outputImage, outputImageSplit);
+
+		cv::normalize(outputImageSplit[1], outputImage, 1.0, 0.0, cv::NORM_MINMAX);
+	}
+}
