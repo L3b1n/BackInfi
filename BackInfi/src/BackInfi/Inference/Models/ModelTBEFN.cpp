e128ad2cbf1db8b281b9d09ebdcb3e5d3e18a3b1
L3b1n <binleo07@icloud.com>
2024-08-27 18:27:09 +0300

    Add TBEFN Model class for inference

---
+++ BackInfi/src/BackInfi/Inference/Models/ModelTBEFN.cpp
@@ -0,0 +1,34 @@
+#include "bcpch.h"
+
+#include "BackInfi/Inference/Models/ModelTBEFN.h"
+
+namespace BackInfi
+{
+	ModelTBEFN::ModelTBEFN()
+	{
+	}
+
+	ModelTBEFN::~ModelTBEFN()
+	{
+	}
+
+	void ModelTBEFN::PostprocessOutput(cv::Mat& output)
+	{
+		// output is already BHWC ...
+		output = output * 255.0; // Convert to 0-255 range
+	}
+
+	cv::Mat ModelTBEFN::GetNetworkOutput(
+		const Activation& activation,
+		const std::vector<std::vector<int64_t>>& outputDims,
+		std::vector<std::vector<float>>& outputTensorValues)
+	{
+		// BHWC
+		uint32_t outputWidth = (int)outputDims[0].at(2);
+		uint32_t outputHeight = (int)outputDims[0].at(1);
+		int32_t outputChannels = CV_MAKE_TYPE(CV_32F, (int)outputDims[0].at(3));
+
+		return cv::Mat(outputHeight, outputWidth, outputChannels, outputTensorValues[0].data());
+	}
+
+}
