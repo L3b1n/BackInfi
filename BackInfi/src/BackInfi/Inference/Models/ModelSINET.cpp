dcbec51826c5fce365eca45ea781a3eb2b534cf0
L3b1n <binleo07@icloud.com>
2024-08-27 18:26:24 +0300

    Add SINet Model class for inference

---
+++ BackInfi/src/BackInfi/Inference/Models/ModelSINET.cpp
@@ -0,0 +1,32 @@
+#include "bcpch.h"
+
+#include "BackInfi/Inference/Models/ModelSINET.h"
+
+namespace BackInfi
+{
+	ModelSINET::ModelSINET()
+	{
+	}
+
+	ModelSINET::~ModelSINET()
+	{
+	}
+
+	void ModelSINET::PrepareInputToNetwork(
+		cv::Mat& resizedImage,
+		cv::Mat& preprocessedImage)
+	{
+		cv::subtract(
+			resizedImage, 
+			cv::Scalar(102.890434, 111.25247, 126.91212),
+			resizedImage
+		);
+		cv::multiply(
+			resizedImage,
+			cv::Scalar(1.0 / 62.93292, 1.0 / 62.82138, 1.0 / 66.355705) / 255.0,
+			resizedImage
+		);
+		Utils::HwcToChw(resizedImage, preprocessedImage);
+	}
+
+}
