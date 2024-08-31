1ba37e9ad21df984de82909da452da28fc5e4e7d
L3b1n <binleo07@icloud.com>
2024-08-31 23:26:58 +0300

    Add new log file name for logger

--- BackInfi/src/BackInfi/Core/Logger.cpp
+++ BackInfi/src/BackInfi/Core/Logger.cpp
@@ -13,7 +13,7 @@ namespace BackInfi
 	{
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("App.log", true));
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("BackInfi.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
