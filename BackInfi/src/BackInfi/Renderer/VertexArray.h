e16dddea00af0f59f3b64bd87ca780cc6c30d0c5
L3b1n <binleo07@icloud.com>
2024-09-01 20:17:04 +0300

    Add VertexArray class for renderer folder

---
+++ BackInfi/src/BackInfi/Renderer/VertexArray.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "BackInfi/Core/Base.h"
+#include "BackInfi/Renderer/Buffer.h"
+
+namespace BackInfi
+{
+
+	class VertexArray
+	{
+	public:
+		virtual ~VertexArray() = default;
+
+		virtual void Bind() const = 0;
+		virtual void UnBind() const = 0;
+
+		virtual void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) = 0;
+		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) = 0;
+
+		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const = 0;
+		virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const = 0;
+
+		static std::shared_ptr<VertexArray> Create();
+	};
+
+}
