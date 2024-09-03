7b708e79f499ba16f0a36d49fef7979a4d1e4094
L3b1n <binleo07@icloud.com>
2024-09-03 23:29:04 +0300

    Remove some functions, move some structs and do so minor fixes

--- BackInfi/src/BackInfi/Inference/BackgroundFilter.h
+++ BackInfi/src/BackInfi/Inference/BackgroundFilter.h
@@ -1,14 +1,25 @@
 #pragma once
 #include <onnxruntime_cxx_api.h>
+#include <cpu_provider_factory.h>
 
-#ifdef _WIN32
+#ifdef BC_PLATFORM_APPLE
+#include <coreml_provider_factory.h>
+#endif
+
+#ifdef BC_PLATFORM_LINUX
+#include <tensorrt_provider_factory.h>
+#endif
+
+#ifdef BC_PLATFORM_WINDOWS
+//#include <dml_provider_factory.h>
 #include <wchar.h>
-#endif // _WIN32
+#endif
 
 #include <glad/glad.h>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
+#include "BackInfi/Inference/GlPrograms.h"
 #include "BackInfi/Inference/Models/ModelRVM.h"
 #include "BackInfi/Inference/Models/ModelSINET.h"
 #include "BackInfi/Inference/Models/ModelSelfie.h"
@@ -22,73 +33,104 @@
 #include "BackInfi/Renderer/FrameBuffer.h"
 #include "BackInfi/Renderer/VertexArray.h"
 
-#include "BackInfi/Inference/FilterData.h"
-#include "BackInfi/Inference/GlPrograms.h"
-
 namespace BackInfi
 {
+
+	// Settings structure
+	struct Settings
+	{
+		bool  EnableThreshold          = false;
+		bool  UseFloatMask             = false;
+		bool  EnableImageSimilarity    = false;
+		float ImageSimilarityThreshold = 0.0f;
+		float Threshold                = 0.0f;
+		float ContourFilter            = 0.0f;
+		float SmoothContour            = 0.0f;
+		float Feather                  = 0.0f;
+		float TemporalSmoothFactor     = 0.0f;
+		int   BlurBackground           = 0;
+		int   MaskEveryXFrames         = 1;
+		uint32_t NumThreads            = 1;
+
+		std::string UseGpu             = USEGPU_CPU;
+		std::string Model              = MODEL_MEDIAPIPE;
+	};
+
+	struct BackgroundRemoval
+	{
+		// ORTModel data
+		std::unique_ptr<Ort::Session>        OnnxSession;
+		std::unique_ptr<Ort::Env>            OnnxEnv;
+		std::vector<Ort::AllocatedStringPtr> InputNames;
+		std::vector<Ort::AllocatedStringPtr> OutputNames;
+		std::vector<Ort::Value>              InputTensor;
+		std::vector<Ort::Value>              OutputTensor;
+		std::vector<std::vector<int64_t>>    InputDims;
+		std::vector<std::vector<int64_t>>    OutputDims;
+		std::vector<std::vector<float>>      OutputTensorValues;
+		std::vector<std::vector<float>>      InputTensorValues;
+
+		// Model part
+		uint32_t               NumThreads;
+		std::string            UseGPU;
+		std::string            ModelSelection;
+		std::unique_ptr<Model> Model;
+
+		// Filter part
+		bool  IsDisabled               = true;
+		bool  UseFloatMask             = false;
+		bool  EnableThreshold          = false;
+		bool  EnableImageSimilarity    = false;
+		float ImageSimilarityThreshold = 0.0f;
+		float Threshold                = 0.0f;
+		float ContourFilter            = 0.0f;
+		float SmoothContour            = 0.0f;
+		float Feather                  = 0.0f;
+		float TemporalSmoothFactor     = 0.0f;
+
+		int BlurBackground             = 0;
+		int MaskEveryXFrames           = 0;
+		int MaskEveryXFramesCount      = 0;
+
+		cv::Mat LastBackgroundMask;
+		cv::Mat LastImage;
+	};
 
 	class BackgroundFilter
 	{
 	public:	
-		BackgroundFilter() {
-			tf = new BackgroundRemovalFilter();
-			std::string instanceName{"background-removal-inference"};
-			tf->env.reset(new Ort::Env(OrtLoggingLevel::ORT_LOGGING_LEVEL_ERROR, instanceName.c_str()));
-			tf->modelSelection = MODEL_MEDIAPIPE;
-		}
+		BackgroundFilter();
+		~BackgroundFilter();
 
-		BackgroundFilter(
-			const int height_,
-			const int width_) 
-				: width(width_),
-				height(height_)
-		{
-			tf = new BackgroundRemovalFilter();
-			std::string instanceName{"background-removal-inference"};
-			tf->env.reset(new Ort::Env(OrtLoggingLevel::ORT_LOGGING_LEVEL_ERROR, instanceName.c_str()));
-			tf->modelSelection = MODEL_MEDIAPIPE;
-		}
+		void SetUp(const int&  height, const int&  width, const bool& blur);
 
-		void filterActivate() { tf->isDisabled = false; }
+		inline void FilterActivate() { m_Filter->IsDisabled = false; }
 
-		void filterDeactivate() { tf->isDisabled = true; }
+		inline void FilterDeactivate() { m_Filter->IsDisabled = true; }
 
-		void loadBackground(const std::string path) {
-			std::string backgroundImagePath = std::filesystem::current_path().append(path).string();
-			background = cv::imread(backgroundImagePath);
-			cv::resize(background, background, cv::Size(width, height));
-			cv::cvtColor(background, background, cv::COLOR_BGR2RGBA);
-			cv::flip(background, background, 0);
-		}
+		void LoadBackground(const std::string path);
 
-		void filterUpdate(const bool enableThreshold_, const float threshold_ = 0.025,
-			const float contourFilter_ = 0.025, const float smoothContour_ = 0.05,
-			const float feather_ = 0.05, const int maskEveryXframes_ = 1,
-			const int64_t blurBackground_ = 0, const std::string useGpu_ = USEGPU_CPU,
-			const std::string model_ = MODEL_MEDIAPIPE, const uint32_t numThreads_ = 1);
+		void FilterUpdate(const Settings& settings);
 
-		void filterVideoTick(const int height, const int width,
-			const int type, unsigned char* data);
+		void FilterVideoTick(const cv::Mat& imageBGR);
 
-		void blendSegmentationSmoothing(const double combine_with_previous_ratio_);
+		void BlendSegmentationSmoothing(const double combine_with_previous_ratio_);
 
-		//void blendBackgroundAndForeground(unsigned char* data);
-		cv::Mat blendBackgroundAndForeground();
+		cv::Mat BlendBackgroundAndForeground(const cv::Mat& inputData);
 
-		bool GlSetup(const int mask_channel);
+		inline cv::Mat GetMask() { return m_BackgroundMask; }
 
-		GLuint VAO;
+		bool GlSetup();
 
 	private:
-		int width = 0;
-		int height = 0;
-		cv::Mat background;
-		cv::Mat current_mask;
-		cv::Mat previous_mask;
-		const cv::Vec3b recolor = { 255, 0, 0 };
+		int                                    m_Width;
+		int                                    m_Height;
+		bool                                   m_Blur;
+		cv::Mat                                m_Background;
+		cv::Mat                                m_PreviousMask;
+		cv::Mat                                m_BackgroundMask;
 
-		BackgroundRemovalFilter *tf;
+		std::unique_ptr<BackgroundRemoval>     m_Filter;
 
 		std::shared_ptr<BackInfi::Shader>      m_Shader;
 
@@ -98,11 +140,6 @@ namespace BackInfi
 		std::shared_ptr<BackInfi::Texture>     m_MaskTexture;
 		std::shared_ptr<BackInfi::Texture>     m_InputTexture;
 		std::shared_ptr<BackInfi::Texture>     m_BackgroundTexture;
-
-		cv::Mat BufferToMat();
-		cv::Mat TextureToMat(GLuint texture_id);
-
-		void processImageForBackground(const cv::Mat& imageBGRA, cv::Mat& backgroundMask);
 
 		inline cv::Vec3b Blend(const cv::Vec3b& color1, const cv::Vec3b& color2,
 			float weight, int invert_mask,
