acdbac778099e56348322d895d992c9fa1c86d96
L3b1n <binleo07@icloud.com>
2024-09-04 14:35:06 +0300

    Add defines for platforms and define bind event macro

--- BackInfi/src/BackInfi/Core/Base.h
+++ BackInfi/src/BackInfi/Core/Base.h
@@ -6,6 +6,23 @@
 	#else
 		#error "x86 Builds are not supported!"
 	#endif
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define BC_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define BC_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#error "Unknown Apple platform!"
+	#endif
 #elif defined(__linux__)
 	#define BC_PLATFORM_LINUX
 	#error "Linux is not supported!"
@@ -32,6 +49,8 @@
 #define BC_STRINGIFY_MACRO(x) #x
 
 #define BIT(x) (1 << x)
+
+#define BC_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 #include "BackInfi/Core/Assert.h"
 #include "BackInfi/Core/Logger.h"
