bf2b8e95a28fc99e050384d3b9d69a2565ccc06a
L3b1n <binleo07@icloud.com>
2024-09-07 16:14:43 +0300

    Add Layer and LayerStack classes

---
+++ BackInfi/src/BackInfi/Core/Layer.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "BackInfi/Core/Base.h"
+#include "BackInfi/Core/TimeStep.h"
+
+#include "BackInfi/Events/Event.h"
+
+namespace BackInfi
+{
+
+	// Layer
+	// ------------------------------------------------------------------------
+	class Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer() = default;
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate(TimeStep ts) {}
+		virtual void OnImGuiRender() {}
+		virtual void OnEvent(Event& event) {}
+
+		const std::string& GetName() const { return m_DebugName; }
+
+	protected:
+		std::string m_DebugName;
+	};
+
+	// LayerStack
+	// ------------------------------------------------------------------------
+	class LayerStack
+	{
+	public:
+		LayerStack() = default;
+		~LayerStack();
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* overlay);
+		void PopLayer(Layer* layer);
+		void PopOverlay(Layer* overlay);
+
+		std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
+		std::vector<Layer*>::iterator end() { return m_Layers.end(); }
+		std::vector<Layer*>::reverse_iterator rbegin() { return m_Layers.rbegin(); }
+		std::vector<Layer*>::reverse_iterator rend() { return m_Layers.rend(); }
+
+		std::vector<Layer*>::const_iterator begin() const { return m_Layers.begin(); }
+		std::vector<Layer*>::const_iterator end()	const { return m_Layers.end(); }
+		std::vector<Layer*>::const_reverse_iterator rbegin() const { return m_Layers.rbegin(); }
+		std::vector<Layer*>::const_reverse_iterator rend() const { return m_Layers.rend(); }
+
+	private:
+		uint32_t m_LayerIndex = 0;
+		std::vector<Layer*> m_Layers;
+	};
+
+}
