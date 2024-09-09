44ee0a8a3b10ea0403ca4f525314d42a010dc805
L3b1n <binleo07@icloud.com>
2024-09-09 16:47:10 +0300

    Add BackInfi.h file which containce all includes for client side

---
+++ BackInfi/src/BackInfi.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "BackInfi/Core/Base.h"
+
+#include "BackInfi/Core/Application.h"
+#include "BackInfi/Core/Layer.h"
+#include "BackInfi/Core/Logger.h"
+#include "BackInfi/Core/Assert.h"
+
+#include "BackInfi/Core/Timestep.h"
+
+#include "BackInfi/Core/KeyCode.h"
+#include "BackInfi/Core/MouseCode.h"
+
+// ---Renderer------------------------
+#include "BackInfi/Renderer/Renderer.h"
+
+#include "BackInfi/Renderer/Buffer.h"
+#include "BackInfi/Renderer/Shader.h"
+#include "BackInfi/Renderer/Framebuffer.h"
+#include "BackInfi/Renderer/Texture.h"
+#include "BackInfi/Renderer/VertexArray.h"
+// -----------------------------------
