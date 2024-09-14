768a997fc6025958ef97f09cab1e0b1990e2e639
L3b1n <binleo07@icloud.com>
2024-09-14 15:50:20 +0300

    Add support of TimeInspector for GlShader class and move some functions to Utils namespace

--- BackInfi/src/Platform/OpenGL/GlShader.cpp
+++ BackInfi/src/Platform/OpenGL/GlShader.cpp
@@ -19,12 +19,69 @@ namespace BackInfi
 			BC_CORE_ASSERT(false, "Unknown shader type!");
 			return 0;
 		}
+
+		std::string ReadFile(const std::string& filepath)
+		{
+			std::string result;
+			std::ifstream in(filepath, std::ios::in | std::ios::binary); // ifstream closes itself due to RAII
+			if (in)
+			{
+				in.seekg(0, std::ios::end);
+				size_t size = in.tellg();
+				if (size != -1)
+				{
+					result.resize(size);
+					in.seekg(0, std::ios::beg);
+					in.read(&result[0], size);
+				}
+				else
+				{
+					BC_CORE_ERROR("Could not read from file '{0}'", filepath);
+				}
+			}
+			else
+			{
+				BC_CORE_ERROR("Could not open file '{0}'", filepath);
+			}
+
+			return result;
+		}
+
+		std::tuple<const char*, const char*> PreProcess(const std::string& source)
+		{
+			std::string vertexSrc, fragmentSrc;
+			const char* typeToken = "#type";
+			size_t typeTokenLength = strlen(typeToken);
+			size_t pos = source.find(typeToken, 0); //Start of shader type declaration line
+			while (pos != std::string::npos)
+			{
+				size_t eol = source.find_first_of("\r\n", pos); //End of shader type declaration line
+				BC_CORE_ASSERT(eol != std::string::npos, "Syntax error");
+				size_t begin = pos + typeTokenLength + 1; //Start of shader type name (after "#type " keyword)
+				std::string type = source.substr(begin, eol - begin);
+				BC_CORE_ASSERT(Utils::ShaderTypeFromString(type), "Invalid shader type specified");
+
+				size_t nextLinePos = source.find_first_not_of("\r\n", eol); //Start of shader code after shader type declaration line
+				BC_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
+				pos = source.find(typeToken, nextLinePos); //Start of next shader type declaration line
+
+				if (Utils::ShaderTypeFromString(type) == GL_VERTEX_SHADER)
+					vertexSrc = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
+				else
+					fragmentSrc = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
+			}
+
+			return { vertexSrc.c_str(), fragmentSrc.c_str() };
+		}
+
 	}
 
 	GlShader::GlShader(const std::string& filepath)
 	{
-		auto source = ReadFile(filepath);
-		auto [vertexCode, fragmentCode] = PreProcess(source);
+		BC_PROFILE_FUNC();
+
+		auto source = Utils::ReadFile(filepath);
+		auto [vertexCode, fragmentCode] = Utils::PreProcess(source);
 
 		GLuint vertex;
 		GLuint fragment;
@@ -82,6 +139,8 @@ namespace BackInfi
 		const std::string& fragmentCode)
 			: m_Name(name)
 	{
+		BC_PROFILE_FUNC();
+
 		const char* vShaderCode = vertexCode.c_str();
 		const char* fShaderCode = fragmentCode.c_str();
 
@@ -116,16 +175,22 @@ namespace BackInfi
 
 	GlShader::~GlShader()
 	{
+		BC_PROFILE_FUNC();
+
 		GlShader::deleteShader(*this);
 	}
 
 	void GlShader::Bind() const
 	{
+		BC_PROFILE_FUNC();
+
 		glUseProgram(m_RendererID);
 	}
 
 	void GlShader::UnBind() const
 	{
+		BC_PROFILE_FUNC();
+
 		glUseProgram(0);
 	}
 
@@ -139,6 +204,8 @@ namespace BackInfi
 
 	GLint GlShader::GetUniformLocation(const std::string& name) const
 	{
+		BC_PROFILE_FUNC();
+
 		if (m_UmapUniformLocationCache.find(name) != m_UmapUniformLocationCache.end())
 			return m_UmapUniformLocationCache[name];
 
@@ -173,104 +240,66 @@ namespace BackInfi
 		}
 	}
 
-	std::string GlShader::ReadFile(const std::string& filepath)
-	{
-		std::string result;
-		std::ifstream in(filepath, std::ios::in | std::ios::binary); // ifstream closes itself due to RAII
-		if (in)
-		{
-			in.seekg(0, std::ios::end);
-			size_t size = in.tellg();
-			if (size != -1)
-			{
-				result.resize(size);
-				in.seekg(0, std::ios::beg);
-				in.read(&result[0], size);
-			}
-			else
-			{
-				BC_CORE_ERROR("Could not read from file '{0}'", filepath);
-			}
-		}
-		else
-		{
-			BC_CORE_ERROR("Could not open file '{0}'", filepath);
-		}
-
-		return result;
-	}
-
-	std::tuple<const char*, const char*> GlShader::PreProcess(const std::string& source)
-	{
-		std::string vertexSrc, fragmentSrc;
-		const char* typeToken = "#type";
-		size_t typeTokenLength = strlen(typeToken);
-		size_t pos = source.find(typeToken, 0); //Start of shader type declaration line
-		while (pos != std::string::npos)
-		{
-			size_t eol = source.find_first_of("\r\n", pos); //End of shader type declaration line
-			BC_CORE_ASSERT(eol != std::string::npos, "Syntax error");
-			size_t begin = pos + typeTokenLength + 1; //Start of shader type name (after "#type " keyword)
-			std::string type = source.substr(begin, eol - begin);
-			BC_CORE_ASSERT(Utils::ShaderTypeFromString(type), "Invalid shader type specified");
-
-			size_t nextLinePos = source.find_first_not_of("\r\n", eol); //Start of shader code after shader type declaration line
-			BC_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
-			pos = source.find(typeToken, nextLinePos); //Start of next shader type declaration line
-
-			if (Utils::ShaderTypeFromString(type) == GL_VERTEX_SHADER)
-				vertexSrc = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
-			else
-				fragmentSrc = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
-		}
-
-		return { vertexSrc.c_str(), fragmentSrc.c_str()};
-	}
-
 	void GlShader::SetInt(const std::string& name, const int& value) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = GetUniformLocation(name);
 		glUniform1i(location, value);
 	}
 
 	void GlShader::SetBool(const std::string& name, const bool& value) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = GetUniformLocation(name);
 		glUniform1i(location, static_cast<int>(value));
 	}
 
 	void GlShader::SetFloat(const std::string& name, const float& value) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = GetUniformLocation(name);
 		glUniform1f(location, value);
 	}
 
 	void GlShader::SetVec2f(const std::string& name, const glm::vec2& value) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = GetUniformLocation(name);
 		glUniform2f(location, value.x, value.y);
 	}
 
 	void GlShader::SetVec3f(const std::string& name, const glm::vec3& value) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = GetUniformLocation(name);
 		glUniform3f(location, value.x, value.y, value.z);
 	}
 
 	void GlShader::SetVec4f(const std::string& name, const glm::vec4& value) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = GetUniformLocation(name);
 		glUniform4f(location, value.x, value.y, value.z, value.w);
 	}
 
 	void GlShader::SetIntArray(const std::string& name, int* values, uint32_t count) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1iv(location, count, values);
 	}
 
 	void GlShader::SetMat4(const std::string& name, const glm::mat4& matrix) const
 	{
+		BC_PROFILE_FUNC();
+
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 	}
